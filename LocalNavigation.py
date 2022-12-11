f7a9283e7bc4a81e8525875e2c0f553481a52a95
Marco <marco.pontarolo@epfl.ch>
2022-12-11 16:42:48 +0100

    Update LocalNavigation.py

--- LocalNavigation.py
+++ LocalNavigation.py
@@ -6,32 +6,29 @@ from ekf import ExtendedKalmanFilter
 from PathPlanning import Global_navigation
 import math
 
+# Constants 
 obstThrL = 500      # low obstacle threshold to switch state 1->0
 obstThrH = 900      # high obstacle threshold to switch state 0->1
 obstSpeedGain = [0.06, 0.04, 0, -0.04, -0.06]  # gain for motor speed necessary to avoid obstacle
-turn = -1
-spLeft = 50
-spRight =50
-current_target_idx = 0
+
+# Variable initialization 
+turn    = -1           # Choice of turning when an obstacle is detected: 0 for counter-clockwise and 1 for clockwise 
+spLeft  = 50         
+spRight = 50
+current_target_idx = 0     # Index stored of the nex target 
 current_plan = 0           # 0=A* algorithm, 1=obstacle avoidance
-obst  = [0,0,0,0,0]  # measurements from left and right prox sensors
-#### Need to define them directly as global vairables. Can't define them AFTER as global variables
 
-#@tdmclient.notebook.sync_var 
-def obstacleAvoidance(position_x, position_y,prox_value,path):
+def obstacleAvoidance(position_x, position_y, prox_value, path):
     """
-    This function is used to detect an obstacle, not present while doing the path planning, and overcome it without colliding.
-    The Thymio will continue to turn until he reaches the first target after the obstacle, from his global path planning.
-    :param obst: a row vector of 5 values from the proximity sensors in front of the Thymio 
-    :param kalman_position: actual postion of the robot 
-    :param path: matrix composed by coordinates (x,y) of the global path planning
-    :param saved_pos: index from 
+    This function is used to store the position when a local obstacle is detected.
+    :param position_x: X coordinate of the current position 
+    :param position_y: Y coordinate of the current position
+    :param prox_value: proximity sensors value
+    :param path: matrix containing all the coordinates calculated from A star algorthm 
     """
-    global obst
-
-    obst = [prox_value[0], prox_value[1], prox_value[2], prox_value[3], prox_value[4]]
-    # store the kalman_position from path before a local obstacle is detected
-    saved_pos = checkState(position_x, position_y,prox_value)
+    
+    # store the position from path before a local obstacle is detected
+    saved_pos = checkPlan(position_x, position_y, prox_value)
     saved_pos_idx = 0
 
     # store the index of the kalman_position from path before a local obstacle is detected
@@ -41,80 +38,83 @@ def obstacleAvoidance(position_x, position_y,prox_value,path):
             saved_pos_idx = i        
     return saved_pos_idx
 
-#@tdmclient.notebook.sync_var
-def checkState(position_x, position_y,prox_value):
+def checkPlan(position_x, position_y, prox_value):
     """
-    Returns the state of the Thymio which indicate whether to follow the path planning 
+    Check if the plan of the Thymio has changed, which indicate whether to follow the path planning 
     from the A* algorithm or to avoid a local obstacle 
-    :param kalman_position: the real time kalman_position of the Thymio  
-    :param obstThrH: the value which the 5 proximity sensors exceed when they detect an object (14 cm)
-    :param obstThrL: the value which indicate if the 5 proximity sensors don't detect anything 
-    :return state: 0=A* algorithm, 1=obstacle avoidance
-    :return saved_pos: kalman_position of the Thymio before finding an obstacle 
+    :param position_x: X coordinate of the current position 
+    :param position_y: Y coordinate of the current position
+    :param prox_value: proximity sensors value
+    :return saved_pos: position of the Thymio before finding an obstacle 
     """
-    global obst, current_plan, obstThrH, obstThrL
-
-    obst = [prox_value[0], prox_value[1], prox_value[2], prox_value[3], prox_value[4]]
+    global current_plan, obstThrH, obstThrL
 
     if current_plan == 0: 
         # switch from goal tracking to obst avoidance if obstacle detected
-        if (max(obst) > obstThrH):
+        if (max(prox_value[0:5]) > obstThrH):
             current_plan = 1
             saved_pos = [position_y, position_x]                        
             return saved_pos
     elif current_plan == 1:
         # switch from obst avoidance to goal tracking if obstacle got unseen 
-        if max(obst) < obstThrL:
+        if max(prox_value[1:6]) < obstThrL:
             current_plan = 0
             saved_pos = 0
             return saved_pos
         
-#@tdmclient.notebook.sync_var
-def motionControl(kalman_position, path, square_size, prox_value): ## state and current_target_position should be global
-    global saved_pos_idx, obstSpeedGain,current_plan, current_target_idx, spLeft,spRight, turn
+def motionControl(kalman_position, path, square_size, prox_value):
+    """
+    Check if the plan of the Thymio has changed, which indicate whether to follow the path planning 
+    from the A* algorithm or to avoid a local obstacle 
+    :param kalman_position: a vector containing coordinates x and y and angle of the Thymio inside the map
+    :param path: contains all the coordinates of the optimal path from the start position to the goal 
+    :param square_size: 
+    :param prox_value: proximity sensors value
+    :return spRight: speed of the right motor 
+    :return spLeft: speed of the left motor
+    """
+    global obstSpeedGain, current_plan, current_target_idx, spLeft, spRight, turn
 
-    thymio_speed = 100
-    range_angle = 15
-    turn_factor = 3
+    thymio_speed = 100    
+    range_angle = 10     # acceptable error angle in degrees for deciding if the direction of the Thymio is good 
+    turn_factor = 3      # experimental gain for turning
 
     position_x, position_y, position_angle = kalman_position
-    position_angle = position_angle*180/np.pi
-
-    # position_x = round(position_x/square_size)
-    # position_y = round(position_y/square_size)
-    # checkState(position_x, position_y, prox_value)
-    # print("current plan:",current_plan)
-    # print("Prox values",prox_value)
+    position_angle = position_angle*180/np.pi    # conversion of the angle from radians to degrees
+    
+    # set the right speed of the motors depending on the situation
     if current_plan == 0:
         saved_pos_idx = obstacleAvoidance(position_x, position_y, prox_value, path)
-        if current_plan:
-            return(0,0)
+        #if current_plan:
+        #    return(0,0)
+        
         if current_target_idx >= path.shape[0]:
             current_target_idx = path.shape[0]-1
             return (0,0)
-
+        
+        # stop when the goal is reached
         if (np.abs([position_y, position_x]-path[current_target_idx])<= np.array([1,1])).all():
-            #([position_y, position_x] == path[current_target_idx]).all():
             current_target_idx += 1
             if current_target_idx >= path.shape[0]:
                 current_target_idx = path.shape[0]-1
                 return (0,0)
-
+            
+        # calculate the target angle in degrees using Thymio position and next target position.
         targetAngle = math.atan2((path[current_target_idx][0] - position_y),(path[current_target_idx][1] - position_x))*180/np.pi %(360)
-
-        diff = position_angle - targetAngle
-        # print("diff before:",diff)
+        # difference of the angle between Thymio orientation and taget angle
+        diff = position_angle - targetAngle     
+        
+        # maintain the difference in a range [-180,180] 
         if (diff>180) : diff = diff-360 
         if (diff<-180) : diff = 360+diff
-        # print("diff after", diff)
-
+        
+        # go straight if the difference between the two angles is less than 15
         if abs(diff) < range_angle:
             diff = 0
         motor_left_speed = int(thymio_speed + diff*turn_factor)
         motor_right_speed = int(thymio_speed - diff*turn_factor)
 
-        #spLeft, spRight = motor_left_speed, motor_right_speed
-        return(motor_right_speed,motor_left_speed)
+        return motor_right_speed, motor_left_speed
         
     elif current_plan == 1:
         obst = [prox_value[0], prox_value[1], prox_value[2], prox_value[3], prox_value[4]]
@@ -123,70 +123,39 @@ def motionControl(kalman_position, path, square_size, prox_value): ## state and 
             turn = turn
         else:    
             turn = ((obst[3] + obst[4]) > (obst[0] + obst[1]))
-        # print("turn:",turn)
-        # true if turning clockwise, false if turning counter-clockwise
-        # for i in range(5):
-        #     spLeft += int(prox_value[i] * obstSpeedGain[i]/10)
-        #     spRight += int(prox_value[i] * obstSpeedGain[4 - i]/10)
+
         # turn clockwise if proximity sensors value is below the threshold low
         if turn==1:
             spLeft = -50
             spRight = 50
             if max(obst[3:5]) < obstThrL:
-                print("sensors:",prox_value)
                 spLeft = 200
                 spRight = 120
-                # saved_pos_idx = obstacleAvoidance(position_x, position_y,prox_value,path)
                 # continue turning until the original path is not found
                 for i in range(4,np.shape(path)[0]-saved_pos_idx):
-                    #if [position_y, position_x] == [path[saved_pos_idx+i][0], path[saved_pos_idx+i][1]]:
                     if (np.abs([position_y, position_x] - path[saved_pos_idx+i]) <= np.array([1,1])).all():
                         current_plan = 0
                         turn = -1
                         current_target_idx = saved_pos_idx + i + 1
                         break
-                # if [position_y, position_x] == [path[saved_pos_idx][0], path[saved_pos_idx][1]]:
-                #     current_plan = 0
-                # if [position_y, position_x] == [path[saved_pos_idx+1][0], path[saved_pos_idx+1][1]]:
-                #     current_plan = 0
-                # if [position_y, position_x] == [path[saved_pos_idx+2][0], path[saved_pos_idx+2][1]]:
-                #     current_plan = 0
+
         # turn counterclockwise if proximity sensors value is below the threshold low
         if turn ==0:
             spLeft = 50
             spRight = -50
             if max(obst[0:2]) < obstThrL:
-                # print("sensors:",prox_value)
                 spLeft = 120
                 spRight = 200
-                # saved_pos_idx = obstacleAvoidance(position_x, position_y,prox_value,path)
+                # continue turning until the original path is not found
                 for i in range(4,np.shape(path)[0]-saved_pos_idx):
-                    #if [position_y, position_x] == [path[saved_pos_idx+i][0], path[saved_pos_idx+i][1]]:
                     if (np.abs([position_y, position_x] - path[saved_pos_idx+i]) <= np.array([1,1])).all():
                         current_plan = 0
                         turn = -1
                         current_target_idx = saved_pos_idx + i + 1
                         break
-                    # continue turning until the original path is not found
-                # if [position_y, position_x] == [path[saved_pos_idx][0], path[saved_pos_idx][1]]:
-                #     current_plan = 0
-                # if [position_y, position_x] == [path[saved_pos_idx+1][0], path[saved_pos_idx+1][1]]:
-                #     current_plan = 0
-                # if [position_y, position_x] == [path[saved_pos_idx+2][0], path[saved_pos_idx+2][1]]:
-                #     current_plan = 0
+                        
         if current_target_idx == len(path):
             spLeft = 0
             spRight = 0
         # motor control
         return(spRight,spLeft)
-        #motor_left_target = spLeft
-        #motor_right_target = spRight
-
-
-def currentPositionIndex(kalman_position, path):
-    global current_target_idx
-
-    if kalman_position == path[current_target_idx+1]:
-        current_target_idx += 1
-
-
